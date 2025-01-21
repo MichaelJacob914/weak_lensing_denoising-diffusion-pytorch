7edd73c74d9a88b3135aa661161ce173378f7792
MichaelJacob914 <102377228+MichaelJacob914@users.noreply.github.com>
2025-01-20 19:47:37 -0500

    Update TrainModel.py

--- TrainModel.py
+++ TrainModel.py
@@ -138,7 +138,7 @@ trainer = Trainer(
     path, 
     train_batch_size = 16,
     train_lr = 8e-5,
-    save_and_sample_every = 1000,
+    save_and_sample_every = 10000,
     train_num_steps = 80000,         # total training steps
     gradient_accumulate_every = 2,    # gradient accumulation steps
     ema_decay = 0.995,                # exponential moving average decay
